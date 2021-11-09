0a91f6c393c06de7a22cb3aaecff3d3b5a2a8c61
vinicius-vs <vinicius_stumpf@edu.univali.br>
2021-11-08 23:12:32 -0300

    Criando os repositorio filhos

--- Condominios_Sol_Nascente/Data/Models/Funcionario.cs
+++ Condominios_Sol_Nascente/Data/Models/Funcionario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    class Funcionario
+    public class Funcionario:BaseModel
     {
         public Condominio condominio { get; set; }
         public string cpf { get; set; }
